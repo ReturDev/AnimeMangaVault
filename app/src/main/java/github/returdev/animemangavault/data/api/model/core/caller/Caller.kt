1b3aa5bbff03987bfd594608705bb7e1d20d2b09
DESKTOP-OT04G7O\Sergio <sergiomm50@gmail.com>
2023-09-21 19:42:07 +0200

    Create the Caller interface.
    Represents an interface for making asynchronous calls.

---
+++ app/src/main/java/github/returdev/animemangavault/data/api/model/core/caller/Caller.kt
@@ -0,0 +1,19 @@
+package github.returdev.animemangavault.data.api.model.core.caller
+
+import retrofit2.Call
+
+/**
+ * Represents an interface for making asynchronous calls.
+ */
+interface Caller {
+
+    /**
+     * Executes a given [Call] and returns the result of the call.
+     *
+     * @param hasNetworkConnection Indicates whether there is an active network connection.
+     * @param call The [Call] to execute.
+     * @return The result of the call.
+     */
+    suspend fun <R> executeCall(hasNetworkConnection : Boolean, call : Call<R>) : R
+
+}
