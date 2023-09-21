f7afb70afc1b1210d2d941318e9f173dbf188116
DESKTOP-OT04G7O\Sergio <sergiomm50@gmail.com>
2023-09-21 23:22:39 +0200

    Create the RetrofitModule for dependence injection.

---
+++ app/src/main/java/github/returdev/animemangavault/data/api/di/RetrofitModule.kt
@@ -0,0 +1,52 @@
+package github.returdev.animemangavault.data.api.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import github.returdev.animemangavault.BuildConfig.BASE_API_URL
+import github.returdev.animemangavault.data.api.service.ApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.time.Duration
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    private const val MAX_TIMEOUT_SECONDS = 3L
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client : OkHttpClient): Retrofit {
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_API_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(Duration.ofSeconds(MAX_TIMEOUT_SECONDS))
+            .readTimeout(Duration.ofSeconds(MAX_TIMEOUT_SECONDS))
+            .build()
+
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit) : ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+
+}
