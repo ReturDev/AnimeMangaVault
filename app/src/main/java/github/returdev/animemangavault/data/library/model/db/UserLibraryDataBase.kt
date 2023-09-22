0cbc91164b943ad82d1a3ed71589d3267b5a39de
DESKTOP-OT04G7O\Sergio <sergiomm50@gmail.com>
2023-09-23 01:41:45 +0200

    Create the user library database.

---
+++ app/src/main/java/github/returdev/animemangavault/data/library/model/db/UserLibraryDataBase.kt
@@ -0,0 +1,28 @@
+package github.returdev.animemangavault.data.library.model.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import github.returdev.animemangavault.data.library.dao.AnimeLibraryDao
+import github.returdev.animemangavault.data.library.dao.MangaLibraryDao
+import github.returdev.animemangavault.data.library.model.entity.AnimeLibraryEntity
+import github.returdev.animemangavault.data.library.model.entity.MangaLibraryEntity
+
+/**
+ * Room database class that defines the database and its associated DAOs.
+ */
+@Database(entities = [AnimeLibraryEntity::class, MangaLibraryEntity::class], version = 1)
+abstract class UserLibraryDataBase : RoomDatabase() {
+    /**
+     * Retrieves the DAO for accessing anime-related data in the library.
+     *
+     * @return The DAO for anime library data.
+     */
+    abstract fun getAnimeLibraryDao() : AnimeLibraryDao
+
+    /**
+     * Retrieves the DAO for accessing manga-related data in the library.
+     *
+     * @return The DAO for manga library data.
+     */
+    abstract fun getMangaLibraryDao() : MangaLibraryDao
+}
