3d53d218ea38d992cc242aab98f721ef9d99114c
DESKTOP-OT04G7O\Sergio <sergiomm50@gmail.com>
2023-09-23 01:42:28 +0200

    Create the UserLibraryModule for the dependence injection.

---
+++ app/src/main/java/github/returdev/animemangavault/data/library/di/UserLibraryModule.kt
@@ -0,0 +1,39 @@
+package github.returdev.animemangavault.data.library.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import github.returdev.animemangavault.data.library.dao.AnimeLibraryDao
+import github.returdev.animemangavault.data.library.dao.MangaLibraryDao
+import github.returdev.animemangavault.data.library.model.db.UserLibraryDataBase
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object UserLibraryModule {
+
+    private const val USER_LIBRARY_DATABASE_NAME = "visual_media_library"
+
+    @Provides
+    @Singleton
+    fun provideUserLibraryDB(@ApplicationContext context: Context) : UserLibraryDataBase {
+        return Room.databaseBuilder(context, UserLibraryDataBase::class.java, USER_LIBRARY_DATABASE_NAME).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAnimeLibraryDao(database: UserLibraryDataBase): AnimeLibraryDao {
+        return database.getAnimeLibraryDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMangaLibraryDao(database: UserLibraryDataBase) : MangaLibraryDao {
+        return database.getMangaLibraryDao()
+    }
+
+}
