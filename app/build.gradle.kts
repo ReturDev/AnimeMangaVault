e2d8167f6fcf11c330e1d6d9aa8c7d22bbdd3279
DESKTOP-OT04G7O\Sergio <sergiomm50@gmail.com>
2023-09-22 23:51:20 +0200

    Create a custom test runner and add it into gradle.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "github.returdev.animemangavault.di.CustomTestRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
