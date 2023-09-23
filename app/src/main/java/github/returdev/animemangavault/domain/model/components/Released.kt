ae39816d9201f6d66e2c774fef31ef3c833e12ad
DESKTOP-OT04G7O\Sergio <sergiomm50@gmail.com>
2023-09-23 15:23:22 +0200

    Create ImageUrl and Released components for the Anime and Manga models.
    - ImageUrl: Created a sealed class representing different image URLs with subclasses for small, normal, and large images.
    
    - Released: Introduced a data class to store release date information with options for both starting and ending dates. Included a custom `toString()` function to format and display the date range.

---
+++ app/src/main/java/github/returdev/animemangavault/domain/model/components/Released.kt
@@ -0,0 +1,27 @@
+package github.returdev.animemangavault.domain.model.components
+
+import android.icu.text.DateFormat
+import android.icu.text.SimpleDateFormat
+import android.icu.util.Calendar
+import java.util.Locale
+
+/**
+ * Data class representing the release date information.
+ *
+ * @property from The starting date of the release.
+ * @property to The ending date of the release (nullable).
+ */
+data class Released(
+    val from : Calendar,
+    val to : Calendar?
+){
+
+    override fun toString(): String {
+        val dateFormat = DateFormat.getPatternInstance("dd/mm/yyyy HH:mm", Locale.getDefault())
+
+        val fromFormatted = dateFormat.format(from)
+        val toFormatted = to?.let { dateFormat.format(it) } ?: "??/??/??"
+
+        return "$fromFormatted - $toFormatted"
+    }
+}
