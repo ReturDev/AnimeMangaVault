1d6a82f67797049ed8d080186effb65fb17b2299
DESKTOP-OT04G7O\Sergio <sergiomm50@gmail.com>
2023-09-26 01:47:19 +0200

    Create use cases for manga interactions with the repository.

---
+++ app/src/main/java/github/returdev/animemangavault/domain/library/manga/DeleteMangaFromUserLibraryUseCase.kt
@@ -0,0 +1,15 @@
+package github.returdev.animemangavault.domain.library.manga
+
+import github.returdev.animemangavault.data.library.repository.AnimeLibraryRepository
+import github.returdev.animemangavault.data.library.repository.MangaLibraryRepository
+import javax.inject.Inject
+
+class DeleteMangaFromUserLibraryUseCase @Inject constructor(
+    private val mangaLibraryRepository: MangaLibraryRepository
+) {
+
+    operator fun invoke(mangaId : Int) : Int{
+        return mangaLibraryRepository.removeManga(mangaId)
+    }
+
+}
