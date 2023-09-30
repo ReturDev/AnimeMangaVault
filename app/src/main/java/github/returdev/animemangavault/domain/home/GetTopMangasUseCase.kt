2c921a232fdab32d8268094f3f592d1bca6f5ac2
DESKTOP-OT04G7O\Sergio <sergiomm50@gmail.com>
2023-09-30 22:14:44 +0200

    Create use cases for fetching home data.

---
+++ app/src/main/java/github/returdev/animemangavault/domain/home/GetTopMangasUseCase.kt
@@ -0,0 +1,29 @@
+package github.returdev.animemangavault.domain.home
+
+import github.returdev.animemangavault.core.model.core.filters.manga.MangaTypeFilters
+import github.returdev.animemangavault.core.network.NetworkConnectivity
+import github.returdev.animemangavault.data.api.model.manga.MangaSearchApiResponse
+import github.returdev.animemangavault.data.api.repository.AnimeApiRepository
+import github.returdev.animemangavault.data.api.repository.MangaApiRepository
+import github.returdev.animemangavault.domain.model.reduced.ReducedManga
+import javax.inject.Inject
+
+class GetTopMangasUseCase @Inject constructor(
+    private val mangaApiRepository: MangaApiRepository,
+    private val networkConnectivity: NetworkConnectivity
+) {
+
+    suspend operator fun invoke(
+        page : Int,
+        limit : Int,
+        type : MangaTypeFilters? = null
+    ): List<ReducedManga> {
+        return mangaApiRepository.getTopManga(
+            page = page,
+            limit = limit,
+            type = type,
+            networkState = networkConnectivity.networkState
+        )
+    }
+
+}
