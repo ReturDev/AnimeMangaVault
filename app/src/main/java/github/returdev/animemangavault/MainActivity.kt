86992e999b4d59ae6e81a51aeac049746352c22b
DESKTOP-OT04G7O\Sergio <sergiomm50@gmail.com>
2023-10-01 22:46:49 +0200

    Create Routes and Navigation.

--- app/src/main/java/github/returdev/animemangavault/MainActivity.kt
+++ app/src/main/java/github/returdev/animemangavault/MainActivity.kt
@@ -10,6 +10,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import github.returdev.animemangavault.ui.navigation.AMVNavigation
 import github.returdev.animemangavault.ui.theme.AnimeMangaVaultTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +27,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    AMVNavigation()
+
                     Greeting("Android")
                 }
             }
